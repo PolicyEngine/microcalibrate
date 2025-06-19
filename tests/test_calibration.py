a509f4ce7e89ed8eb169c7683b00aa30c8e3a53f
baogorek <baogorek@gmail.com>
2025-06-19 07:52:45 -0400

    adding (failing) test of contradiction testing

--- tests/test_calibration.py
+++ tests/test_calibration.py
@@ -2,9 +2,11 @@
 Test the calibration process.
 """
 
-from src.microcalibrate.calibration import Calibration
 import numpy as np
 import pandas as pd
+
+from src.microcalibrate.calibration import Calibration
+from src.microcalibrate.utils import simulate_contradictory_data
 
 
 def test_calibration_basic() -> None:
@@ -15,7 +17,7 @@ def test_calibration_basic() -> None:
     data = pd.DataFrame(
         {
             "age": random_generator.integers(18, 70, size=100),
-            "income": random_generator.normal(40000, 50000, size=100),
+            "income": random_generator.normal(40000, 10000, size=100),
         }
     )
     weights = np.ones(len(data))
@@ -73,7 +75,7 @@ def test_calibration_harder_targets() -> None:
     data = pd.DataFrame(
         {
             "age": random_generator.integers(18, 70, size=100),
-            "income": random_generator.normal(40000, 50000, size=100),
+            "income": random_generator.normal(40000, 10000, size=100),
         }
     )
     weights = np.ones(len(data))
@@ -121,3 +123,62 @@ def test_calibration_harder_targets() -> None:
         rtol=0.01,  # relative tolerance
         err_msg="Calibrated totals do not match target values",
     )
+
+
+def test_calibration_responds_to_contradiction() -> None:
+    """Test that calibration performance degrades predicably with increased contradiction"""
+
+    results = []
+    for contradiction_factor in [.1, .2]:
+
+        sample_df, totals = simulate_contradictory_data(
+            T = 6000,
+            k = 3,
+            c = contradiction_factor,
+            n = 30
+        )
+
+        metrics_matrix = pd.DataFrame(
+            {
+                "y_all": sample_df["y_ij"],
+                "y_stratum_1": (
+                    (sample_df["stratum_id"] == 1).astype(float) * sample_df["y_ij"]
+                ),
+                "y_stratum_2": (
+                    (sample_df["stratum_id"] == 2).astype(float) * sample_df["y_ij"]
+                ),
+                "y_stratum_3": (
+                    (sample_df["stratum_id"] == 3).astype(float) * sample_df["y_ij"]
+                ),
+            }
+        )
+
+        calibrator = Calibration(
+            loss_matrix=metrics_matrix,
+            weights=sample_df['w_ij'],
+            targets=np.hstack([totals['T_official'], totals['S_star_official']])
+        )
+
+        calibrator.calibrate()
+
+        new_weights = calibrator.weights
+        new_estimates = np.matmul(metrics_matrix.T, new_weights)
+
+        overall_loss = (totals['T_official'] - new_estimates["y_all"]) ** 2
+        strata_loss = (
+            (totals['S_star_official'][0] - new_estimates["y_stratum_1"]) ** 2
+            + (totals['S_star_official'][1] - new_estimates["y_stratum_2"]) ** 2
+            + (totals['S_star_official'][2] - new_estimates["y_stratum_3"]) ** 2
+        )
+
+        results.append([contradiction_factor, overall_loss, strata_loss])
+
+    results_df = pd.DataFrame(
+        results,
+        columns=["contradiction_factor", "overall_loss", "strata_loss"]
+    )
+
+    assert results_df["overall_loss"].is_monotonic_increasing, "Overall loss is not increasing"
+    assert results_df["strata_loss"].is_monotonic_increasing, "Strata loss is not increasing"
+
+
